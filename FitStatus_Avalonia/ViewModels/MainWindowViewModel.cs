2d28910c4cfa0795a1efcf9832a6c3f86aed6176
Teheze <kropla.2001@o2.pl>
2023-03-09 05:08:18 +0100

    initial window design

--- FitStatus_Avalonia/ViewModels/MainWindowViewModel.cs
+++ FitStatus_Avalonia/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,18 @@
-﻿namespace FitStatus_Avalonia.ViewModels;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+// using CommunityToolkit.Mvvm.ComponentModel;
+// using CommunityToolkit.Mvvm.Input;
+
+namespace FitStatus_Avalonia.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    public string Bmi => "Twoje BMI wynosi: 24.9";
 }
